da007f7efb9eea4fb8d1764f2952edefa45b4a66
hvle <ha.hong.viet.le@cgi.com>
2015-12-04 00:05:19 +0100

    le @ComponentScan via @SpringBootApplication dans fr.bappr.sbt.Application.java scan déjà ce package et les sous packages

--- src/main/java/fr/bappr/sbt/config/WebConfig.java
+++ src/main/java/fr/bappr/sbt/config/WebConfig.java
@@ -1,10 +1,8 @@
 package fr.bappr.sbt.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class WebConfig {
 	
 }
