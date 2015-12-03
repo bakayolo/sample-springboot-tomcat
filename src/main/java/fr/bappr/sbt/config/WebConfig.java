5b9f9a3f1d694c7d189fa83b938f95fedbea0d28
hvle <ha.hong.viet.le@cgi.com>
2015-12-04 00:03:36 +0100

    @SpringBootApplication => @EnableAutoConfiguration => instanciation de WebMvcAutoConfiguration (c'est l'auto config de Spring Boot) @SpringBootApplication => @EnableWebMvc automatiquement ajouté dès que spring-webmvc dans le classpath + auto instanciation du DispatcherServlet

--- src/main/java/fr/bappr/sbt/config/WebConfig.java
+++ src/main/java/fr/bappr/sbt/config/WebConfig.java
@@ -1,13 +1,10 @@
 package fr.bappr.sbt.config;
 
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan
-public class WebConfig extends WebMvcAutoConfiguration {
+public class WebConfig {
 	
 }
